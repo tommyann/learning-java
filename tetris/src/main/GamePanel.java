436ce70fa341ac753529ef08eed2f2a0ed8e2668
Tommaso Annibale <tommaso.annibale@outlook.it>
2025-02-15 00:19:52 +0100

    First part of tetris

--- tetris/src/main/GamePanel.java
+++ tetris/src/main/GamePanel.java
@@ -5,10 +5,12 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements Runnable{
     //panel settings
     public static final int WIDTH = 1280;
     public static final int HEIGHT = 720;
+    final int FPS = 60;
+    Thread gameThread;
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
